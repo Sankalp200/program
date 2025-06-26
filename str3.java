de6a89e7b82f696eac8a737ec27d9d7a2ac2ce24
Sankalp singh <156959389+Sankalp200@users.noreply.github.com>
2025-06-26 22:15:44 +0530

    commit

---
+++ str3.java
@@ -0,0 +1,14 @@
+
+class Str3
+{
+    public static void main(String args[])
+    {
+       //use of stringbuilder
+       StringBuilder sb=new StringBuilder("h");
+       sb.append("e");//str=str+"e";
+       sb.append("l");//str=str+"l";
+       sb.append("o");
+       System.out.println(sb);
+    }
+
+}
